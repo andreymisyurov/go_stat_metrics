e588e7efbcbdb5a5330968d527129f61577e9fbc
amisyurov <amisyurov.c@arlo.com>
2024-12-11 23:10:43 +0500

    Fix data type. from int to float

--- src/ex00/main.go
+++ src/ex00/main.go
@@ -13,11 +13,11 @@ func main() {
 	sc.Scan()
 	txt := sc.Text()
 	parts := strings.Fields(txt)
-	var nums []int
+	var nums []float64
 	var sum float64
 
 	for i := 0; i < len(parts); i++ {
-		num, err := strconv.Atoi(parts[i])
+		num, err := strconv.ParseFloat(parts[i], 64)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s - is not a number\n", parts[i])
 			os.Exit(1)
@@ -25,7 +25,7 @@ func main() {
 		sum += float64(num)
 		nums = append(nums, num)
 	}
-	fmt.Println(sum / float64(len(nums)))
+	fmt.Printf("Mean: %.2f\n", sum / float64(len(nums)))
 }
 
 
